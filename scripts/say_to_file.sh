c1d085bab6aad2f7081306232944ade58c9a09dd
Alex Nichol <unixpickle@gmail.com>
2024-12-05 00:52:38 -0500

    fix

--- scripts/say_to_file.sh
+++ scripts/say_to_file.sh
@@ -4,6 +4,7 @@ line=0
 data_dir=/Volumes/MLData2/text2speech
 cat books/*.txt | while read x; do
   if [ -f $data_dir/inputs/line_${line}.txt ]; then
+    line=$((line+1))
     continue
   fi
   echo "$x" | say -o $data_dir/outputs/line_${line}.aiff
